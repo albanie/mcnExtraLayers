da1bd3b6e82480b497d7a5e964c94fde5cbe9a6a
albanie <albanie@robots.ox.ac.uk>
2017-09-24 14:52:38 +0100

    element-wise max derivatives

--- matlab/vl_nnmax.m
+++ matlab/vl_nnmax.m
@@ -1,8 +1,11 @@
-function [y,dx2] = vl_nnmax(M, varargin)
+function varargout = vl_nnmax(M, varargin)
 % VL_NNMAX Element-wise maximum
 %   Y = VL_NNMAX(M, X1, ..., XM) applies the elementwise max operator to
 %   the given M input tensors, each of which should have the same input
 %   dimension.
+%
+%   [DZDX1,..,DZDXM] = VL_NNMAX(M,X1, ...,XM, DZDY) computes the
+%   derivatives of the block projected onto DZDY.
 %
 % Copyright (C) 2017 Samuel Albanie
 % Licensed under The MIT License [see LICENSE.md for details]
@@ -13,8 +16,13 @@ function [y,dx2] = vl_nnmax(M, varargin)
 
   shared = cat(5, ins{:}) ;
   if isempty(dzdy)
-     y = max(shared, [], 5) ;
+     varargout{1} = max(shared, [], 5) ;
   else
-    keyboard % not yet implemented
-    y = vl_nnbilinearsampler(x, grid, dzdy{1}) ;
+    % recompute forward max and insert derivatives at max locations
+    [~,I] = max(shared, [], 5) ;
+    dzdx = zeros(size(shared), 'like', shared) ;
+    offsets = (I - 1) .* numel(dzdy{1}) ;
+    idx = offsets(:) + (1:numel(dzdy{1}))' ;
+    dzdx(idx) = dzdy{1} ;
+    varargout = arrayfun(@(x) {dzdx(:,:,:,:,x)}, 1:M) ;
   end
