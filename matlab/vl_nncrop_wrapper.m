dbc1f58cd21331ac0ae0e76677acebbbcb839557
albanie <albanie@robots.ox.ac.uk>
2017-09-25 13:22:12 +0100

    added supporting tests

--- matlab/vl_nncrop_wrapper.m
+++ matlab/vl_nncrop_wrapper.m
@@ -1,11 +1,11 @@
 function y = vl_nncrop_wrapper(x1, x2, crop, dzdy)
 %VL_NNCROP_WRAPPER AutoNN wrapper for MatConvNet's vl_nncrop
-%  This wrapper computes adaptived crop sizes (as used in the 
+%  This wrapper computes adaptive crop sizes (as used in the 
 %  FCN segmentation framework) to enable direct use of the 
 %  vl_nncrop interface by cropping X1 to match the size of X2 
-
+%
 % Copyright (C) 2017 Samuel Albanie
-% All rights reserved.
+% Licensed under The MIT License [see LICENSE.md for details]
 
   v2 = size(x1,1) - size(x2,1) ;
   u2 = size(x1,2) - size(x2,2) ;
@@ -14,6 +14,6 @@ function y = vl_nncrop_wrapper(x1, x2, crop, dzdy)
   adjCrop = [v2 - v1, v1, u2 - u1, u1] ;
   if nargin < 4
     y = vl_nncrop(x1, adjCrop) ;
-  else  % backward pass
-    y = vl_nncrop(x1, adjCrop, dzdy, inSz) ;
+  else  % backward pass (returned for x1 only)
+    y = vl_nncrop(x1, adjCrop, dzdy) ;
   end
