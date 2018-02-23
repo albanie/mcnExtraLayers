085f0eb154fabce7bd34cd6ca1e6f4e41afe1c97
albanie <albanie@robots.ox.ac.uk>
2018-02-23 20:52:42 +0000

    add cosine similarity

--- matlab/vl_nnaxpy.m
+++ matlab/vl_nnaxpy.m
@@ -2,7 +2,7 @@ function y = vl_nnaxpy(a, x1, x2, varargin)
 % VL_NNAXPY - perform a channelwise multilpy and addition
 %   Y = VL_NNAXPY(A, X1, X2) computes the result Y = A * X1 + X2
 %   where X1 and X2 are HxWxCxN tensors and A is a 1x1xC array.
-%  
+%
 % Copyright (C) 2017 Samuel Albanie
 % Licensed under The MIT License [see LICENSE.md for details]
   [~, dzdy] = vl_argparsepos(struct(), varargin) ;
