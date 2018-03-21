8d3c2bc224fddc2df9af6d74023c36b93b5f9846
albanie <albanie@robots.ox.ac.uk>
2018-03-21 00:31:43 +0000

    cleanup

--- matlab/+dagnn/SoftmaxCELoss.m
+++ matlab/+dagnn/SoftmaxCELoss.m
@@ -2,8 +2,7 @@ classdef SoftmaxCELoss < dagnn.Loss
 
   methods
     function outputs = forward(obj, inputs, params)
-      outputs{1} = vl_nnsoftmaxceloss(inputs{1}, inputs{2}, ...
-                                      'instanceWeights', inputs{3}) ;
+      outputs{1} = vl_nnsoftmaxceloss(inputs{1}, inputs{2}) ;
       n = obj.numAveraged ;
       m = n + size(inputs{1}, 4) ;
       obj.average = (n * obj.average + gather(outputs{1})) / m ;
@@ -11,8 +10,7 @@ classdef SoftmaxCELoss < dagnn.Loss
     end
 
     function [derInputs, derParams] = backward(obj, inputs, params, derOutputs)
-      derInputs{1} = vl_nnsoftmaxceloss(inputs{1}, inputs{2}, derOutputs{1}, ...
-                                       'instanceWeights', inputs{3}) ;
+      derInputs{1} = vl_nnsoftmaxceloss(inputs{1}, inputs{2}, derOutputs{1}) ;
       derInputs{2} = [] ;
       derInputs{3} = [] ;
       derParams = {} ;
