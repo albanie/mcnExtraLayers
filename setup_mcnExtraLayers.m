e5d6b0593c4221caf25b3af1b4dc98cf32bfdbae
albanie <albanie@robots.ox.ac.uk>
2018-04-02 16:33:09 +0100

    update setup

--- setup_mcnExtraLayers.m
+++ setup_mcnExtraLayers.m
@@ -2,4 +2,4 @@ function setup_mcnExtraLayers
 %SETUP_MCNEXTRALAYERS Sets up mcnExtraLayers by adding its folders to the path
 
   root = fileparts(mfilename('fullpath')) ;
-  addpath(root, [root '/matlab'], [root '/utils']) ;
+  addpath(root, [root '/matlab'], [root '/matlab/wrappers'], [root '/utils']) ;
