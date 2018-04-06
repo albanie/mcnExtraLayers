4ee258156923727eb34b71cb18400595834390c7
albanie <albanie@robots.ox.ac.uk>
2018-04-06 23:45:23 +0100

    metrics

---
+++ matlab/+dagnn/ErrorStats.m
@@ -0,0 +1,46 @@
+classdef ErrorStats < dagnn.Loss
+
+  properties (Transient)
+    numClasses = []
+    confusion = 0
+  end
+
+  methods
+    function outputs = forward(obj, inputs, params)
+      [~,predictions] = max(inputs{1}, [], 3) ;
+      predictions = squeeze(gather(predictions)) ;
+      labels = squeeze(gather(inputs{2})) ;
+      obj.confusion = obj.confusion + ...
+        accumarray([labels, predictions], 1, [obj.numClasses obj.numClasses]) ;
+      classDist = sum(obj.confusion, 2)' ;
+      classAccs = diag(obj.confusion)' ;
+      outputs{1} = classAccs ; % perform normalisation later
+      obj.accumulateAverage(classAccs, classDist)  ;
+    end
+
+    function [derInputs, derParams] = backward(obj, inputs, params, derOutputs)
+      derInputs{1} = [] ;
+      derInputs{2} = [] ;
+      derParams = {} ;
+    end
+
+    function accumulateAverage(obj, classAccs, classDist)
+      if obj.ignoreAverage, return ; end
+      n = obj.numAveraged ;
+      m = max(n + classDist, 1e-5) ;
+      classAvgs = bsxfun(@plus, n .* obj.average, classAccs) ./ m ;
+      obj.average = [mean(classAvgs) classAvgs] ;
+      obj.numAveraged = m ;
+    end
+
+    function reset(obj)
+      obj.confusion = 0 ;
+      obj.numAveraged = 0 ;
+      obj.average = zeros(1, obj.numClasses) ;
+    end
+
+    function obj = ErrorStats(varargin)
+      obj.load(varargin) ;
+    end
+  end
+end
